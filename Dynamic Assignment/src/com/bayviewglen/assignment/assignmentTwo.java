69d1a56a47105834a4c58513e260afc2d34d3e8c
jjiabvg <jjia@bayviewglen.ca>
2014-12-07 13:47:09 -0500

    fixed bug
    fixed bug

--- Dynamic Assignment/src/com/bayviewglen/assignment/assignmentTwo.java
+++ Dynamic Assignment/src/com/bayviewglen/assignment/assignmentTwo.java
@@ -5,43 +5,54 @@ import java.util.Scanner;
 
 public class assignmentTwo {
 
-	@SuppressWarnings("unused")
 	public static int Americans(int[] donations){
 
 		int neighborNum = donations.length; 
-		//make 2 array, one contain the first to second last, the other contain second to last 
-		//handles the ring they make 
-		int [] d1 = Arrays.copyOfRange(donations, 0, neighborNum-1); //everything except last donation
-		int [] d2 = Arrays.copyOfRange(donations, 1, neighborNum); //everything except first donation
-		
-		
-		int [] dp = new int[neighborNum-1]; 
-		int [] dp2 = new int[neighborNum-1]; 
-		
-		dp[0] = d1[0];
-		dp[1] = Math.max(d1[0], d1[1]);
-		dp[2] = Math.max(d1[0]+d1[2], d1[1]); 
-		
-		for(int i = 3; i<neighborNum-1;i++){
-			dp[i] = Math.max( dp[i - 2] , dp[i - 3]) + d1[i]; //do it for situation one
+		if(neighborNum == 1){
+			return donations[0]; 
 		}
-		
-		dp2[0] = d2[0];
-		dp2[1] = Math.max(d2[1], d2[0]);
-		dp2[2] = Math.max(d2[0]+d2[2], d2[1]); 
-
-		for(int i = 3; i<neighborNum-1;i++){
-			dp2[i] = Math.max( dp2[i - 2] , dp2[i - 3]) + d2[i];//do it for situation two
+		else if(neighborNum == 2){
+			return Math.max( donations[0], donations[1]); 
 		}
-		int solution; 
-		solution = Math.max(dp[neighborNum-2], dp2[neighborNum-2]); //answer is the maximum
+		else if(neighborNum == 3){
+			return Math.max( donations[0]+donations[2], donations[1]); 
+		}
+		else{
+
+			//make 2 array, one contain the first to second last, the other contain second to last 
+			//handles the ring they make 
+			int [] d1 = Arrays.copyOfRange(donations, 0, neighborNum-1); //everything except last donation
+			int [] d2 = Arrays.copyOfRange(donations, 1, neighborNum); //everything except first donation
+
+
+			int [] dp = new int[neighborNum-1]; 
+			int [] dp2 = new int[neighborNum-1]; 
+
+			dp[0] = d1[0];
+			dp[1] = Math.max(d1[0], d1[1]);
+			dp[2] = Math.max(d1[0]+d1[2], d1[1]); 
+
+			for(int i = 3; i<neighborNum-1;i++){
+				dp[i] = Math.max( dp[i - 2] , dp[i - 3]) + d1[i]; //do it for situation one
+			}
+
+			dp2[0] = d2[0];
+			dp2[1] = Math.max(d2[1], d2[0]);
+			dp2[2] = Math.max(d2[0]+d2[2], d2[1]); 
+
+			for(int i = 3; i<neighborNum-1;i++){
+				dp2[i] = Math.max( dp2[i - 2] , dp2[i - 3]) + d2[i];//do it for situation two
+			}
+			int solution; 
+			solution = Math.max(dp[neighborNum-2], dp2[neighborNum-2]); //answer is the maximum
 
 
 
-		return solution;
+			return solution;
+		}
 	}
 	public static void main(String[] args){
-		
+
 		int[] donations = null; 
 		Scanner scanner = new Scanner(System.in);
 		int length; 
@@ -50,9 +61,9 @@ public class assignmentTwo {
 		donations = new int[length]; 
 		System.out.println("Enter the sequence");
 		System.out.println("Enter one number then hit enter.");
-	    for (int i = 0; i < donations.length; i++) { //load the numbers into an array 
-	    	donations[i] = scanner.nextInt();
-	    }
+		for (int i = 0; i < donations.length; i++) { //load the numbers into an array 
+			donations[i] = scanner.nextInt();
+		}
 
 		System.out.println(Americans(donations)); 
 	}
